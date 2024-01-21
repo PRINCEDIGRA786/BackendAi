b5fdc7b61387fe20bf03318c1396033cd3d6395f
Prince <121872553+PRINCEDIGRA786@users.noreply.github.com>
2024-01-21 21:59:01 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,36 +1,7 @@
-// const mongoose = require('mongoose')
-// const mongoURI = "mongodb://127.0.0.1:27017/apnaai"
-
-// const connectToMongo = () => {
-//     mongoose.connect(mongoURI, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-
-//     });
-
-//     // Get the default connection
-//     const db = mongoose.connection;
-
-//     // Event listener for successful connection
-//     db.on('connected', () => {
-//         console.log('Connected to MongoDB');
-//     });
-
-//     // Event listener for connection error
-//     db.on('error', (err) => {
-//         console.error(`MongoDB connection error: ${err}`);
-//     });
-
-
-// }
-
-// module.exports = connectToMongo;
 
 const mongoose = require('mongoose')
-// const mongoURI = "mongodb://127.0.0.1:27017/netflix"
-// const mongoURI = "mongodb+srv://digraprince7:<IlKqi6D7N9ovvHV6>@cluster0.1qqmyp3.mongodb.net/"
-// const mongoURI = "mongodb+srv://digraprince7:seRx4FH4rpGnyRSG@netflix.6hdiqde.mongodb.net/?retryWrites=true&w=majority"
-const mongoURI = "mongodb+srv://digraprince7:ztuvrtOaG1HwVyT2@apnaai.tdnw4ck.mongodb.net/"
+
+const mongoURI = "mongodb+srv://digraprince7:ztuvrtOaG1HwVyT2@apnaai.tdnw4ck.mongodb.net/?retryWrites=true&w=majority"
 const connectToMongo = async () => {
     // mongoose.set('strictQuery', true)
     try {
